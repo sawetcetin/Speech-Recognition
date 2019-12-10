963005dcdd9c99ad880522e33b2391b438cb3bc6
Samet Çetin <46911606+sawetcetin@users.noreply.github.com>
2019-12-11 00:58:47 +0300

    Speech-Recognition

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Bilgin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Bilgin.Properties.Resources.Bilginrobot_Open;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Bilgin.Properties.Resources.Bilginrobot;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(171, 177);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Bilgin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Bilgin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechRecognitionEngine recoEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer speechSyn = new SpeechSynthesizer();
+        bool izin;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            bilginrobot_ayarlari();
+            izin = true;
+            recoEngine.RecognizeAsync();
+        }
+        void bilginrobot_ayarlari()
+        {
+            string[] ihtimaller = { "Merhaba", "Googleyi aç" , "Facebooka gir" ,  "Youtubeye gir" , "Müslüm baba aç" , "Kapat" , "Merhaba yaz" };
+            Choices seçenekler = new Choices(ihtimaller);
+            Grammar grammer = new Grammar(new GrammarBuilder(seçenekler));
+            recoEngine.LoadGrammar(grammer);
+            recoEngine.SetInputToDefaultAudioDevice();
+            recoEngine.SpeechRecognized += ses_tanıdığında;
+        }
+   
+        private void ses_tanıdığında(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (izin == true)
+            {
+                pictureBox1.Visible = true;
+                izin = false;
+
+                if (e.Result.Text == "Merhaba")
+                {
+                    speechSyn.SpeakAsync("Hello, at your services bay bilgin works");
+                }
+
+                if (e.Result.Text == "Googleyi aç")
+                {
+                    speechSyn.SpeakAsync("Okey, a will ethernet start now.");
+                    System.Diagnostics.Process.Start("C://Program Files (x86)//Google//Chrome//Application//chrome.exe");
+                }
+
+                if (e.Result.Text == "Facebooka gir")
+                {
+                    System.Diagnostics.Process.Start("www.facebook.com/");
+                }
+
+                if (e.Result.Text == "Youtubeye gir")
+                {
+                    System.Diagnostics.Process.Start("www.youtube.com");
+                }
+
+                if (e.Result.Text == "Kapat")
+                {
+                    SendKeys.Send("{^} + {W}");
+                }
+
+                if (e.Result.Text == "Merhaba yaz")
+                {
+                    SendKeys.Send("Merhaba + {ENTER}");
+                }
+
+                if (e.Result.Text == "Müslüm baba aç")
+                {
+                    System.Diagnostics.Process.Start("www.youtube.com/watch?v=0hQCKkqmDlw");
+                }
+            }
+        }
+    }
+}
